31c8c65d08bc2d6ba9a24128b5eef39a44f3aa52
PRAKTIKAN <thoriq.a.taqy@gmail.com>
2026-05-06 08:14:23 +0700

    Menambahkan API Game

---
+++ Modul10_103022400081/Controllers/GameController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+namespace Modul10_103022400081.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class GameController : ControllerBase
+    {
+        private static List<Game> games = new List<Game>
+        {
+            new Game { nama ="Valorant", Developer = "Riot Games", TahunRilis = 2020,Genre = "FPS", Rating= 8.5, Platform=["PC"], Mode= ["Multiplayer"], IsOnline = true, Harga = 0},
+            new Game { nama = "GTA Vt", Developer = "“Rockstar Games", TahunRilis=  2013,Genre= "Open World", Rating = 9.5, Platform=["PC","PS4","PS5","Xbox"], Mode =["Singleplayer", "Multiplayer"], IsOnline =true, Harga =300000},
+            new Game { nama = "“The Witcher 3", Developer = "CD Projekt Red", TahunRilis=  2015,Genre= "RPG", Rating = 9.7, Platform=["PC","PS4","PS5","Xbox","Switch"], Mode =["Singleplayer"], IsOnline =false, Harga =250000}
+        };
+        [HttpGet]
+        public ActionResult<List<Game>> GetAll()
+        {
+            return games;
+        }
+
+        [HttpGet("{index}")]
+        public ActionResult<Game> GetByIndex(int index)
+        {
+            if(index < 0 || index >= games.Count)
+                return NotFound();
+            return games[index];
+        }
+        [HttpPut]
+        public ActionResult<Game> putByIndex(int index)
+        {
+            if (index < 0 || index >= games.Count)
+                return NotFound();
+            return games [index];
+        }
+
+        [HttpPost]
+        public ActionResult <Game> addGame( Game game)
+        {
+            games.Add(game);
+            return Ok(games);
+        }
+        [HttpDelete("{index}")]
+        public ActionResult DeleteGame(int index)
+        {
+            var game = games.FirstOrDefault(g => g.id == index);
+            if (game == null)
+            {
+                return NotFound();
+            }
+            games.Remove(game);
+            return Ok(games);
+        }
+    }
+}
--- Modul10_103022400081/Game.cs
+++ Modul10_103022400081/Game.cs
@@ -1,6 +1,23 @@
-﻿namespace Modul10_103022400081
+﻿using Microsoft.AspNetCore.Routing.Constraints;
+
+namespace Modul10_103022400081
 {
     public class Game
     {
+        public int id { get; set; }
+        public string nama { get; set; }
+        public string Developer { get; set; }
+        public int TahunRilis { get; set; }
+        public string Genre { get; set; }
+        public double Rating { get; set; }
+        public string[] Platform { get; set; }
+        public string[] Mode { get; set; }
+        public Boolean IsOnline { get; set; }
+        public int Harga { get; set; }
+
+        public Game()
+        {
+
+        }
     }
 }
